b430e5ef195cdf26fa14c4b8e7d1706d50221431
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-27 10:59:42 +0200

    added counter to ec2

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "public_ip" {
   value = "${aws_instance.ec2.public_ip}"
 }
 
-output "instance_id" {
-  value = "${aws_instance.ec2.id}"
+output "instance_ids" {
+  value = "${join(",", aws_instance.ec2.*.id)}"
 }
