6c19a5e993cb3a6b3c8218ef3f5d8d21da4e5f20
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-27 10:42:05 +0200

    added counter to ec2

--- main.tf
+++ main.tf
@@ -6,31 +6,34 @@ provider "aws" {
 }
 
 resource "template_file" "yaml" {
+  count = "${var.instance_count}"
   filename = "${var.cloud_init}"
 
   vars {
-    hostname = "${var.hostname}"
+    hostname = "${var.hostname}${count.index+1}"
     internal_domain = "${var.internal_domain}"
   }
 }
 
 resource "aws_instance" "ec2" {
+  count = "${var.instance_cont}"
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   subnet_id = "${var.subnet_id}"
   security_groups = [ "${split(",", var.sg_ids)}" ]
   key_name = "${var.key_name}"
   source_dest_check = false
-  user_data = "${template_file.yaml.rendered}"
+  user_data = "${element(template_file.yaml.*.rendered, count.index)}"
   tags = {
-    Name = "${var.hostname}"
+    Name = "${var.hostname}${count.index+1}"
   }
 }
 
 resource "aws_route53_record" "host" {
+  count = "${var.instance_cont}"
   zone_id = "${var.zone_id}"
-  name = "${var.hostname}"
+  name = "${var.hostname}${count.index+1}"
   type = "A"
   ttl = "300"
-  records = ["${aws_instance.ec2.private_ip}"]
+  records = ["${element(aws_instance.ec2.*.private_ip, count.index)}"]
 }
